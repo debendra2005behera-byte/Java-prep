d886f4641695086a1b989a1242ef791c2e0b59a6
debendra2005behera-byte <debendra2005behera@gmail.com>
2026-09-05 17:02:21 +0530

    Add files via upload

---
+++ Day2.java
@@ -0,0 +1,9 @@
+public class Day2 {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 8;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a % b);
+    }
+}
